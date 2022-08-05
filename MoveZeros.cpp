469442e9fe78bc88b6a90fc92de4b0617fe2d164
kanika mahal <kanikamahal123@gmail.com>
2022-08-05 10:30:54 +0530

    Leetcode 283

---
+++ MoveZeros.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int left=0;
+        int right=0;
+        int temp;
+        while(right<n){
+
+            if(nums[right]==0)
+            {
+               ++right;
+            }
+
+        else{
+            temp = nums[left];
+            nums[left]= nums[right];
+            nums[right]= temp;
+            ++left;
+            ++right;
+        }
+        }
+
+    }
+};
